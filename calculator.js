1b6cb88ee3e334a3904f05ac962ad8b8c6fc8ab9
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-12-22 12:39:26 -0800

    initial commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function calculate(){
+	//Write your code here:
+
+	//Some useful APIs and concepts to consider:
+	//document.getElementById()
+	//Retrieving a value from HTML element
+	//Changing text of HTML from JavaScript
+	//Converting a string into a number
+}
